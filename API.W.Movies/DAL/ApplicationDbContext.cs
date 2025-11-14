926f750b76f8ce6a14fd87e600d29591475d90ef
Daniel <Jordanribba@gmail.com>
2025-11-14 17:03:27 +0100

    Agregar archivos de proyecto.

---
+++ API.W.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.W.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.W.Movies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Sección para crear el dbset de las entidades o modelos
+        public DbSet<Category> Categories { get; set; }
+    }
+}
