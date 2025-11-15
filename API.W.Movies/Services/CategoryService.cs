0075bf33d4983048d22b95d6bdc607ed6f9c732e
Daniel <Jordanribba@gmail.com>
2025-11-15 17:59:16 +0100

    New CategoriesController and CategoriesService Created

---
+++ API.W.Movies/Services/CategoryService.cs
@@ -0,0 +1,84 @@
+﻿using API.W.Movies.DAL.Models;
+using API.W.Movies.DAL.Models.Dtos;
+using API.W.Movies.Repository.IRepository;
+using API.W.Movies.Services.IServices;
+using AutoMapper;
+
+namespace API.W.Movies.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CategoryDto> CreateCategoryAsync(CategoryCreateDto categoryCreateDto)
+        {
+            //Validar si la categoría ya existe
+            var categoryExists = await _categoryRepository.CategoryExistsByNameAsync(categoryCreateDto.Name);
+
+            if (categoryExists)
+            {
+                throw new InvalidOperationException($"Ya existe una categoría con el nombre de '{categoryCreateDto.Name}'");
+            }
+
+            //Mapear el DTO a la entidad
+            var category = _mapper.Map<Category>(categoryCreateDto);
+
+            //Crear la categoría en el repositorio
+            var categoryCreated = await _categoryRepository.CreateCategoryAsync(category);
+
+            if (!categoryCreated)
+            {
+                throw new Exception("Ocurrió un error al crear la categoría.");
+            }
+
+            //Mapear la entidad creada a DTO
+            return _mapper.Map<CategoryDto>(category);
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
+        {
+            // Obtener las categorías del repositorio
+            var categories = await _categoryRepository.GetCategoriesAsync();
+
+            // Mapear toda la colección de una vez
+            return _mapper.Map<ICollection<CategoryDto>>(categories);
+        }
+
+
+        public async Task<CategoryDto> GetCategoryAsync(int id)
+        {
+            // Obtener la categoría del repositorio
+            var category = await _categoryRepository.GetCategoryAsync(id);
+
+            // Mapear toda la colección de una vez
+            return _mapper.Map<CategoryDto>(category);
+        }
+
+        public Task<CategoryDto> UpdateCategoryAsync(int id, Category categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API.W.Movies/Services/IServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using API.W.Movies.DAL.Models;
+using API.W.Movies.DAL.Models.Dtos;
+
+namespace API.W.Movies.Services.IServices
+{
+    public interface ICategoryService
+    {
+        Task<ICollection<CategoryDto>> GetCategoriesAsync();
+        Task<CategoryDto> GetCategoryAsync(int id);
+        Task<CategoryDto> CreateCategoryAsync(CategoryCreateDto categoryDto);
+        Task<CategoryDto> UpdateCategoryAsync(int id, Category categoryDto);
+        Task<bool> DeleteCategoryAsync(int id);
+        Task<bool> CategoryExistsByIdAsync(int id);
+        Task<bool> CategoryExistsByNameAsync(string name);
+    }
+
+}
